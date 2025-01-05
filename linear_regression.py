e907a43829ac570e878a731a96428fa011d32859
camilajaviera <cjmunozn@hotmail.com>
2025-01-05 16:18:00 -0300

    feat(lr): split data into trainig and testing sets

--- linear_regression.py
+++ linear_regression.py
@@ -12,3 +12,6 @@ df.columns = ['period', 'product', 'quantity', 'total']
 # Features and target variables
 X = df['period']
 y = df['quantity']
+
+# Split data into trainig and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
