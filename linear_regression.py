2f3ca283084893bfca63271b64e947885ab7dd5b
camilajaviera <cjmunozn@hotmail.com>
2025-01-05 16:19:41 -0300

    feat(lr): train the model

--- linear_regression.py
+++ linear_regression.py
@@ -15,3 +15,7 @@ y = df['quantity']
 
 # Split data into trainig and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Train the model
+model = LinearRegression()
+model.fit(X_train, y_train)
