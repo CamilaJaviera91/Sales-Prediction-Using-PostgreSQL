50407fe3cc8c0420d880879561cde32c23d12a9c
camilajaviera <cjmunozn@hotmail.com>
2025-01-05 16:16:04 -0300

    feat(lr): import libraries and connection file, also assign names to the columns and create target variables

---
+++ linear_regression.py
@@ -0,0 +1,14 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from query import query
+import pandas as pd
+
+df = pd.DataFrame(query())
+
+# Assign names to the columns
+df.columns = ['period', 'product', 'quantity', 'total']
+
+# Features and target variables
+X = df['period']
+y = df['quantity']
