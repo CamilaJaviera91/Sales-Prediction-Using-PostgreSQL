0c142f4ffd405029752a8047e0df85894785a119
camilajaviera <cjmunozn@hotmail.com>
2025-01-06 18:31:34 -0300

    feat(plot): change linar regression to random forest, because the data wasn't working with the first model.

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,8 @@
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+import seaborn as sns
+
 from query import query
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -23,14 +25,14 @@ df['date'] = pd.to_datetime(df[['year', 'month']].assign(day=1))
 print(df.dtypes)
 
 # Features and target variables
-X = df[['month', 'year']]
+X = df.drop(columns=['quantity', 'total', 'date'])
 y = df['quantity']
 
 # Split data into trainig and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Train the model
-model = LinearRegression()
+model = RandomForestRegressor()
 model.fit(X_train, y_train)
 
 # Predict and evaluate
@@ -39,22 +41,25 @@ mse = mean_squared_error(y_test, predictions)
 print(f"Mean Squared Error: {round(mse, 2)}")
 
 # Visualization
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(12, 8))
 
 # Plot of actual sales
-plt.plot(df['date'], df['quantity'], label='Ventas Reales', marker='o', linestyle='-')
+sns.lineplot(x=df['date'], y=df['quantity'], label='Real Sales', marker='o')
 
 # Prepare the predictions for plotting
 X_test['predicted_quantity'] = predictions
-X_test['date'] = pd.to_datetime(X_test[['year', 'month']].assign(day=1))
+X_test['date'] = df.loc[X_test.index, 'date']
 
-# Plot the predictions
-plt.plot(X_test['date'], X_test['predicted_quantity'], label='Ventas Predichas', linestyle='--', marker='x')
+sns.lineplot(x=X_test['date'], y=X_test['predicted_quantity'], label='Predicted Sales', linestyle='--', marker='o')
+
+# Prepare date range from 2022 to 2023
+dates = pd.date_range(start='2022-01', end='2023-12', freq='MS')
+plt.xticks(dates, labels=[date.strftime('%Y-%m') for date in dates], rotation=45)
 
 # Configure the plot
-plt.legend()
 plt.xlabel('Date')
 plt.ylabel('Sales')
-plt.title('Real sales vs Predictions')
+plt.title('Real Sales vs Predictions')
+plt.legend()
 plt.grid(True)
 plt.show()
